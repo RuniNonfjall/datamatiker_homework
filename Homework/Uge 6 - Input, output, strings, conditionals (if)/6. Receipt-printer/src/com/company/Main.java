93a8c97afc92d001953e75e413a896936770f38b
RuniNonfjall <100132375+RuniNonfjall@users.noreply.github.com>
2022-02-23 01:49:05 +0100

    Last time I swear

---
+++ Homework/Uge 6 - Input, output, strings, conditionals (if)/6. Receipt-printer/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+//Butikkens Navn
+//et unikt nummer (løbenummer for kvitteringen)
+//dags dato
+//kundens navn
+//det samlede beløb der er købt for
+//betalingsmetode (fx kreditkort, mobilepay, kontant, etc.)
+//sælgerens navn
+public class Main {
+    public
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+	    }
+}
