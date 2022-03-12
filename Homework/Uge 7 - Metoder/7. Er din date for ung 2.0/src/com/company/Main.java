890f318c9e97510381a08e96af0bcb2363c18440
RuniNonfjall <100132375+RuniNonfjall@users.noreply.github.com>
2022-03-12 02:34:43 +0100

    Friday Night Diggedarer!

--- Homework/Uge 7 - Metoder/7. Er din date for ung 2.0/src/com/company/Main.java
+++ Homework/Uge 7 - Metoder/7. Er din date for ung 2.0/src/com/company/Main.java
@@ -5,17 +5,12 @@ import java.util.Scanner;
 //Er din date for ung 2.0
 public class Main {
 
-  public static void main(String[] args) {
 
+  public static void main(String[] args) {
     //Version 2.0
     System.out.println("Velkommen til programmet Er din date for ung");
     System.out.println("Dette er den nye og forbedrede version v2.0 \n");
-
-    //Importer Scanne til keyboard
     Scanner keyboard = new Scanner(System.in);
-
-
-    // Indtast dinAlder
     System.out.print("Indtast din alder her: ");
     int dinAlder = keyboard.nextInt();
 
@@ -26,11 +21,10 @@ public class Main {
       System.out.print("Indtast din dates alder her: ");
       int datesAlder = keyboard.nextInt();
 
-
+      // Tal der ikke vil forekomme under normale omstændigheder
       // Hvis i er på samme alder afsluttes rutinen
       // Hvis du er ældre end din partner, hop til del 1
       // Hvis din partner er ældre end dig, hop til del 2
-
 
       // I er på samme alder!
       if (dinAlder == datesAlder) {
@@ -41,14 +35,14 @@ public class Main {
       // Du er ældre end din date! Del 1.
       if (dinAlder > datesAlder) {
 
-        int dinNedreGrænse  = ((dinAlder / 2) + 7);
-        if (dinNedreGrænse <= datesAlder) {
+        int dinNedreGrænse = ((dinAlder / 2) + 7);
+        if (dinNedreGrænse < datesAlder) {
           System.out.println("Du må gerne gå på date med hende! ");
           exit = true;
         }
 
         // Din date er for ung, du må derfor
-        if (dinNedreGrænse > datesAlder) {
+        if (dinNedreGrænse >= datesAlder) {
           System.out.println("Du må ikke gå på date med hende! ");
           System.out.println("Du bliver nød til at finde en der er lidt ældre! ");
         }
@@ -56,7 +50,7 @@ public class Main {
       //Hun er ældre end dig! Del 2.
       if (dinAlder < datesAlder) {
 
-        int dinØvereGrænse = ((dinAlder -7) * 2);
+        int dinØvereGrænse = ((dinAlder - 7) * 2);
         if (dinØvereGrænse >= datesAlder) {
           System.out.println("Du må gerne gå på date med hende! ");
           exit = true;
@@ -70,5 +64,89 @@ public class Main {
       }
     }
 
+
   }
 }
+
+//int minAlder = 16;
+//int maxAlder = 60;
+//Importer Scanne til keyboard
+
+/*
+    boolean firstExit = false;
+
+    while (!firstExit) {
+
+      Scanner keyboard = new Scanner(System.in);
+      System.out.print("Indtast din alder her: ");
+      int dinAlder = keyboard.nextInt();
+        if (dinAlder > maxAlder) {
+        System.out.println("Du har tastet et tal der ikke giver mening prøv Igen! \nIndtast Din alder her: ");
+        }
+        if (dinAlder < minAlder) {
+          System.out.println("Du har tastet et tal der ikke giver mening prøv Igen! \nIndtast Din alder her: ");
+        }
+        // hvis du er yngre end Højst tilladte alder
+        if (dinAlder <= maxAlder) {
+          firstExit = true;
+        }
+        //Hvis du er yngre end Mindst tilladte alder
+        if (dinAlder >= minAlder) {
+          firstExit = true;
+        }
+      }
+      */
+
+
+/*
+    boolean exit = false;
+
+    while (!exit) {
+      // Indtast din datesAlder
+      System.out.print("Indtast din dates alder her: ");
+      int datesAlder = keyboard.nextInt();
+
+      // Tal der ikke vil forekomme under normale omstændigheder
+      // Hvis i er på samme alder afsluttes rutinen
+      // Hvis du er ældre end din partner, hop til del 1
+      // Hvis din partner er ældre end dig, hop til del 2
+
+      // I er på samme alder!
+      if (dinAlder == datesAlder) {
+        System.out.println("Du må gerne gå på date med hende! ");
+        exit = true;
+      }
+
+      // Du er ældre end din date! Del 1.
+      if (dinAlder > datesAlder) {
+
+        int dinNedreGrænse = ((dinAlder / 2) + 7);
+        if (dinNedreGrænse < datesAlder) {
+          System.out.println("Du må gerne gå på date med hende! ");
+          exit = true;
+        }
+
+        // Din date er for ung, du må derfor
+        if (dinNedreGrænse >= datesAlder) {
+          System.out.println("Du må ikke gå på date med hende! ");
+          System.out.println("Du bliver nød til at finde en der er lidt ældre! ");
+        }
+      }
+      //Hun er ældre end dig! Del 2.
+      if (dinAlder < datesAlder) {
+
+        int dinØvereGrænse = ((dinAlder - 7) * 2);
+        if (dinØvereGrænse >= datesAlder) {
+          System.out.println("Du må gerne gå på date med hende! ");
+          exit = true;
+        }
+        // Din date er for gammel, du må derfor
+        if (dinØvereGrænse < datesAlder) {
+          System.out.println("Du må ikke gå på date med hende! ");
+          System.out.println("Du bliver nød til at finde en der er lidt yngre! ");
+        }
+
+      }
+    }
+    */
+
